45f597e27a0435ca34bb61a78b3814045dfdbf62
juanpasq <manu_pasquali@hotmail.com>
2018-01-24 18:12:47 -0300

    agregado fix y estadisticas pages

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { EstadisticasPage } from '../estadisticas/estadisticas';
+import { FixturePage } from '../fixture/fixture';
+
 
 @Component({
   templateUrl: 'tabs.html'
@@ -12,6 +15,8 @@ export class TabsPage {
   tab1Root = HomePage;
   tab2Root = AboutPage;
   tab3Root = ContactPage;
+  tab4Root = FixturePage;
+  tab5Root = EstadisticasPage;
 
   constructor() {
 
